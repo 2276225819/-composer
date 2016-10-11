9700135de5b9a5834446df0cfd17076549148311
2276225819 <227625819@qq.com>
2016-10-11 22:50:32 +0800

    bugfix

--- composer#create.bat
+++ composer#create.bat
@@ -6,8 +6,7 @@
 php %self%/composer.phar create-project --repository-url https://packagist.phpcomposer.com  %vendor% temp %version%
 for /D %%i in (temp\*) do (move %%i .)
 move temp\* .
-rd temp
-php %self%/composer.phar config repositories.packagist composer "https://packagist.phpcomposer.com"
+rd temp 
 call %self%/composer#install.bat
 pause
 pause
--- composer#install.bat
+++ composer#install.bat
@@ -1,4 +1,5 @@
 @call set_path.bat  
 del "composer.lock"
+php %self%/composer.phar config repositories.packagist composer "https://packagist.phpcomposer.com"
 php %self%/composer.phar install 
 pause
