2a582739f67cc1ad4773110f58cc1e03da1ef82f
2276225819 <227625819@qq.com>
2016-10-13 11:52:51 +0800

    bug

--- composer#install.bat
+++ composer#install.bat
@@ -1,5 +1,5 @@
 @call set_path.bat  
 del "composer.lock"
-php %self%/composer.phar config repositories.packagist composer "https://packagist.phpcomposer.com"
+php %self%/composer.phar config repositories.0 composer "https://packagist.phpcomposer.com"
 php %self%/composer.phar install 
 pause
