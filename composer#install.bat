58709c9f636c88d8082aa50c468c2efb72b30f19
2276225819 <227625819@qq.com>
2016-10-13 19:19:43 +0800

    ins

--- composer#install.bat
+++ composer#install.bat
@@ -1,5 +1,7 @@
 @call set_path.bat  
-del "composer.lock"
-php %self%/composer.phar config repositories.0 composer "https://packagist.phpcomposer.com"
+@set /p choice=delete composer.lock(Y/N)
+@if %%choice%==Y (del "composer.lock" )
+@if %%choice%==y (del "composer.lock" )
+php %self%/composer.phar config repositories.packagist composer "https://packagist.phpcomposer.com"
 php %self%/composer.phar install 
 pause
