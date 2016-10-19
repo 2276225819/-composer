9f06d66cfb6c2c3ae941ffb21ea879d1a207798b
2276225819 <227625819@qq.com>
2016-10-19 17:19:28 +0800

    bug fix

--- composer#install.bat
+++ composer#install.bat
@@ -1,7 +1,7 @@
 @call set_path.bat  
 @set /p choice=delete composer.lock(Y/N)
-@if %choice%==Y (del "composer.lock" )
-@if %choice%==y (del "composer.lock" )
+@if "%choice%"=="Y" (del "composer.lock" )
+@if "%choice%"=="y" (del "composer.lock" )
 
 
 @findstr repositories composer.json>nul||php %self%/composer.phar config repositories.packagist composer "https://packagist.phpcomposer.com"
