a8e44575d085ffeec8a2f8b5e62f94fb199bd803
2276225819 <227625819@qq.com>
2016-10-27 16:48:47 +0800

    if

--- composer#install.bat
+++ composer#install.bat
@@ -1,9 +1,10 @@
 @call set_path.bat  
+@if exist "composer.lock" (goto:DEL) else (goto:THEN)
+:DEL
 @set /p choice=delete composer.lock(Y/N)
 @if "%choice%"=="Y" (del "composer.lock" )
 @if "%choice%"=="y" (del "composer.lock" )
-
-
+:THEN
 @findstr repositories composer.json>nul||php %self%/composer.phar config repositories.packagist composer "https://packagist.phpcomposer.com"
 php %self%/composer.phar install 
 pause
