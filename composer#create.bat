4d14a0d7445102c32b39520854efd74c3c558545
2276225819 <2276225819@qq.com>
2016-11-03 13:39:45 +0800

    update composer#create.bat

--- composer#create.bat
+++ composer#create.bat
@@ -3,7 +3,7 @@
 @set /P vendor=" > "
 @echo input project version:
 @set /P version=" > "  
-php %self%/composer.phar create-project --repository-url https://packagist.phpcomposer.com  %vendor% temp %version%
+php %self%/composer.phar create-project --no-install --repository-url https://packagist.phpcomposer.com  %vendor% temp %version% 
 for /D %%i in (temp\*) do (move %%i .)
 move temp\* .
 rd temp
