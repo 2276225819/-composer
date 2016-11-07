2e7e16e66c9ef0963177f5f027124a7274d4e10f
2276225819 <2276225819@qq.com>
2016-11-07 14:15:56 +0800

    cd %~dp0

--- set_path.bat
+++ set_path.bat
@@ -1,6 +1,6 @@
 @if defined SET_PATH goto :eof 
-@set SET_PATH=defined 
 cd %~dp0
+@set SET_PATH=defined 
 @echo[set ws=createobject("wscript.shell")  >a.vbs 
 @echo[set lnk=ws.CreateShortcut("%cd%/php.lnk"^) >>a.vbs  
 @echo[wscript.echo(  left(lnk.TargetPath, len(lnk.TargetPath)-7 ) )  >>a.vbs 
