4e74375ed813e966e3492a2120be22cd8201b3b1
2276225819 <2276225819@qq.com>
2016-11-10 18:26:09 +0800

    update server.bat

--- server.bat
+++ server.bat
@@ -2,7 +2,7 @@
 @echo input port:
 @set /P port=" > "  
 :LOOP
-cls
-php artisan serve --port %port%
-pause
+@cls
+@if exist "artisan" ( php artisan serve --port %port% ) else ( php -Slocalhost:%port% )
+@ping 127.0.0.1 -n 2 >nul   
 goto :LOOP
