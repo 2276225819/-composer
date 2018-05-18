f845e31cc89a4721135901c05cac7016ceeb2ebb
2276225819@qq.com <2276225819@qq.com>
2018-05-18 22:21:49 +0800

    modified bat

--- composer.bat
+++ composer.bat
@@ -1,2 +1,2 @@
 @call %~dp0/init.bat
-@php -n -d extension=%PATH_PHP%/ext/php_openssl.dll %~dp0/bin/composer.phar %1 %2 %3 %4 %5 %6 %7 %8 %9
+@php -n -d extension=%PATH_PHP%/ext/php_openssl.dll %~dp0/bin/composer.phar %*
--- phpcs.bat
+++ phpcs.bat
@@ -1,2 +1,2 @@
 @call %~dp0/init.bat
-@php -n %~dp0/bin/phpcs.phar %1 %2 %3 %4 %5 %6 %7 %8 %9
+@php -n %~dp0/bin/phpcs.phar %*
